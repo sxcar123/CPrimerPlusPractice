e230f3c134ed25918a87ad32b2b0349f4ce36921
sxcar123 <sxcar123@pku.edu.cn>
2020-02-22 09:07:51 +0800

    想要和艾洛君爱爱

---
+++ chapter12/12.8.7/Aiello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int dick=520;
+void Fuck(void)
+{
+	extern char *pussy;
+	printf("Winter's pussy is %s",pussy);
+}
---
+++ chapter12/12.8.7/Winter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+char *pussy;
+int suck(void)
+{
+	extern int dick;
+	return dick;
+}
+int main(void)
+{
+	pussy="warm and soft.";
+	printf("%d\n",suck());
+	Fuck();
+	return 0;
+}
